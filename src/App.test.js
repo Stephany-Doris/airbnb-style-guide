3d9137ce9a64a427215d6eef567e6b923c920e48
Stephany <stephany.awino@twiga.com>
2021-03-01 20:44:27 +0300

    add config files

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders source code link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/View source code/i);
   expect(linkElement).toBeInTheDocument();
 });
