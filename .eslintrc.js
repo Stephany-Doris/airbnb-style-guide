102ed631b12e14734b04c3c374a614daf36d4f06
Stephany <stephany.awino@twiga.com>
2021-03-11 22:14:14 +0300

    add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,15 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/errors',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'airbnb',
+    'prettier'
+  ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true
@@ -13,5 +21,28 @@ module.exports = {
     sourceType: 'module'
   },
   plugins: ['react'],
-  rules: {}
+  rules: {
+    'react/prop-types': 0,
+    'no-console': 1,
+    'react-hooks/rules-of-hooks': 2,
+    'react-hooks/exhaustive-deps': 1,
+    'import/no-unresolved': [2, { caseSensitive: false }],
+    'react/jsx-filename-extension': 0,
+    'react/jsx-props-no-spreading': [1, { extensions: ['.js', '.jsx'] }],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'import/prefer-default-export': 1,
+    'react/no-array-index-key': 1,
+    'no-shadow': 1,
+    camelcase: 1,
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        labelComponents: [],
+        labelAttributes: [],
+        controlComponents: [],
+        assert: 'either',
+        depth: 25
+      }
+    ]
+  }
 };
